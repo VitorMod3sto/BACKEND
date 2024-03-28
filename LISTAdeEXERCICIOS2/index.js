ba0e42b2dd00ead364074682ec234021214820f4
VitorMod3sto <vitorebrunomodesto@gmail.com>
2024-03-27 21:32:43 -0300

    LISTA2, 27/03

--- LISTAdeEXERCICIOS2/index.js
+++ LISTAdeEXERCICIOS2/index.js
@@ -24,6 +24,43 @@ app.post('/exercicio1', (req, res) => {
     res.json(resposta)
 })
 
+/* 7. Faça uma api para ler o código e o preço de 15 produtos, calcular e escrever:
+    • O maior preço lido; e
+    • A média aritmética dos preços dos produtos.
+*/
+app.post('/exercicio7', (req, res) => {
+console.log(req.body)
+
+let listaProdutos = []
+console.log(listaProdutos)
+
+req.body.forEach(produto => {
+    listaProdutos.push(produto)
+});
+
+let maiorPreco = 0
+listaProdutos.forEach(produto => {
+    console.log( 'preço do produto:' + produto.preco)
+    if (produto.preco > maiorPreco ){
+        maiorPreco = produto.preco
+        console.log("Maior preço: " + maiorPreco)
+    }
+})
+
+let somaDosprecos = 0
+listaProdutos.forEach(produto => {
+    somaDosprecos = somaDosprecos + produto.preco
+    console.log("soma dos preços: " + somaDosprecos)
+})
+
+let media = somaDosprecos/listaProdutos.length
+// resposta:
+res.json({
+    maiorPreco: maiorPreco,
+    media : media
+})
+})
+
 app.listen(port, () => {
     console.log("Api iniciando! Rodando em http://localhost:3000")
  })
